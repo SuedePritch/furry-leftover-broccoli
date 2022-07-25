4477979c92b60d802a00bead1b26023be4ce0d6e
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-25 12:03:53 -0400

    react v17, basic backend  setup

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+// const { User, Book } = require('../models');
+// const { signToken } = require('../utils/auth');
+// const { AuthenticationError } = require('apollo-server-express');
+
+
+
+const resolvers = {
+  Query: {
+    
+  },
+  Mutation: {
+    
+
+    },
+};
+
+module.exports = resolvers;
