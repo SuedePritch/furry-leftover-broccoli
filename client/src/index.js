92759aec71f4e90750e561278f036dcb19e23832
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-27 12:10:12 -0400

    setup colours, added title font

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './assets/fonts/Seagram.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
