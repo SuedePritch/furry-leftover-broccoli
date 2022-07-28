89f07967f4562632904e23bfffd0ed28cd038799
Stephen Fudge <sfudge@gmail.com>
2022-07-28 12:42:30 -0400

    created single category page with link in navbar

---
+++ client/src/pages/CategoryPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SingleCategory from "../components/SingleCategory";
+
+const CategoryPage = () => {
+  return (
+    <div className="container">
+      <SingleCategory/>
+    </div>
+  )
+}
+
+export default CategoryPage;
