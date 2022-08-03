5fb3659ff827591a413defab2eea7c4d83d16e08
James Pritchard <james.edward.pritchard@gmail.com>
2022-08-03 11:13:40 -0400

    seventh

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+app.get('/', (req, res) => {  res.sendFile(path.join(__dirname, '../client/build/index.html'));});
+app.get('/*', (req, res) => {  res.sendFile(path.join(__dirname, '../client/build/index.html'));})
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
