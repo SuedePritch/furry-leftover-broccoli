fc698b97473d71204cd631ead618f6238ae21ad9
James Pritchard <james.edward.pritchard@gmail.com>
2022-08-09 13:45:28 -0400

    Revert "stripe is implemented and working!"

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,15 +1,12 @@
 import React from "react";
 import AllCategories from "../components/AllCategories";
 import AllProducts from "../components/AllProducts";
-import Cart from "../components/Cart";
 
 const CategoryPage = () => {
   return (
     <div className="container">
-        <Cart/>
         <AllCategories/>
         <AllProducts/>
-        
     </div>
   )
 }
